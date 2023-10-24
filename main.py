64118d4c82a733c3f1ad0abec466dcf66643034c
ChrMackie <cmackie4736@gmail.com>
2023-10-24 13:41:26 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# define teh variables 'guess' and 'answer'
+guess = 42  #you can replace this with any integer value
+answer = 42  #you can replace this wih any integer value
+
+#Check if the guess is equal to the answer
+if guess == answer:
+    print("You got it first time")
+else:
+    #provide feedback on the guess
+    if guess < answer
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    #get a new guess from the user
+    guess = int(input())
+    #check if the new guess is correct
+    if guess == answer
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+
+
+
