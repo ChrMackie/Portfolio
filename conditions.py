f2590b70bd41c60876144bf4f59dfe19e1a6b7e3
ChrMackie <cmackie4736@gmail.com>
2023-10-30 18:47:34 -0500

    work

---
+++ conditions.py
@@ -0,0 +1,16 @@
+x = 0 # this is my loop counter
+while x<=5:
+    print("This is x: ",x)
+    x=x+1 # increment x to control the condition
+
+x = 0 # this is my loop counter
+while x<10:
+    print(x**2)
+    x=x+1 #increment x to control the condition
+
+x = 0
+while x<1000000:
+    print(x)
+    if x==5:
+        break
+    x+=1
--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 age = int(input("How old are you?"))
-#simplified comparison - if 16 <=age<=65.
-if age >= 16 and age <= 65:
+# simplified comparison - if 16 <=age<=65.
+if 16 <= age <= 65:
     print("Have a good day at work")
- else:
-     print("Enjoy your free time!")
+else:
+    print("Enjoy your free time!")
 
 print("-" * 80)
