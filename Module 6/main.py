63707b982e5495ace79c8c24ba0052282ba78149
ChrMackie <cmackie4736@gmail.com>
2023-10-31 14:00:45 -0500

    work

---
+++ Module 6/main.py
@@ -0,0 +1,9 @@
+#import the module
+import my_module
+
+#use the greet function
+my_module.greet("Alice")
+
+#Create an instance of the Person class
+alice = my_module.Person("Alice")
+alice.say_hello()
